ae50c8258235d8dff3f8bdb0625e3acc1a3c76cb
RiddMa <riddma@qq.com>
2021-04-19 21:56:46 +0800

    what

---
+++ cmake-build-debug/CMakeFiles/ShabbyToys.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ShabbyToys.dir/linux/list.c.o"
+  "ShabbyToys"
+  "ShabbyToys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ShabbyToys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
